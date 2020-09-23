abe2ce26733c6371bf11447d37f420d3a7020607
Shuyang He <maxhe2019@gmail.com>
2020-09-23 00:19:56 -0700

    09/23

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,13 @@ import thunk from "redux-thunk";
 let store;
 const midwares = [thunk];
 
-if (window.__REDUX_DEVTOOLS_EXTENSION__) {
+if (
+  !(
+    window.__REDUX_DEVTOOLS_EXTENSION__ || window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+) {
+  store = createStore(reducer, applyMiddleware(...midwares));
+} else {
   store = createStore(
     reducer,
     compose(
@@ -14,8 +20,6 @@ if (window.__REDUX_DEVTOOLS_EXTENSION__) {
         window.__REDUX_DEVTOOLS_EXTENSION__()
     )
   );
-} else {
-  store = createStore(reducer, applyMiddleware(...midwares));
 }
 
 export default store;
