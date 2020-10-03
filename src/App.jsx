f8987c8f872c6cc111446affc525a22d27dbda17
Shuyang He <maxhe2019@gmail.com>
2020-10-03 11:43:40 -0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import SectionTitle from "./components/SectionTitle/SectionTitle";
 import About from "./components/About/About";
 import Education from "./components/Education/Education";
 import Experience from "./components/Experience/Experience";
-import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
 
 class App extends Component {
   render() {
