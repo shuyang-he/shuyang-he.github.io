2d209bf872e1998e1e4c103371f4c7508eeb9df6
Shuyang He <maxhe2019@gmail.com>
2020-10-04 17:42:49 -0700

    redux extension production

--- src/store/index.js
+++ src/store/index.js
@@ -3,22 +3,16 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
-const composeEnhancers = composeWithDevTools({});
-
 let store;
 const midwares = [thunk];
 
 if (window.__REDUX_DEVTOOLS_EXTENSION__) {
   store = createStore(
     reducer,
-    composeWithDevTools(
-      applyMiddleware(...midwares)
-      // window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      //   window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    composeWithDevTools(applyMiddleware(...midwares))
   );
 } else {
-  store = createStore(reducer, composeEnhancers(applyMiddleware(...midwares)));
+  store = createStore(reducer, compose(applyMiddleware(...midwares)));
 }
 
 export default store;
