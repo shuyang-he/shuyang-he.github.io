b3e68073e756de3a2819a7294e35538116e049b8
Shuyang He <maxhe2019@gmail.com>
2020-10-04 18:07:28 -0700

    react extension

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,16 @@ import ReactDOM from "react-dom";
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./store/index";
+import { disableReactDevTools } from "./helpers/utils";
 import App from "./App";
 import "normalize.css";
 import "./reset.scss";
 import "./typography.scss";
 import "./base.scss";
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
 
 ReactDOM.render(
   <Provider store={store}>
