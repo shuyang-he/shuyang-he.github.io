11b8b4505153f8c6e90f8566b4388c5ed68f86e8
Shuyang He <maxhe2019@gmail.com>
2020-10-18 02:03:20 -0700

    express redirect

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req, res) => {
+  if (req.statusCode === 200) {
+    res.status(200).send(path(resolve(__dirname, "/public/index.html")));
+  } else {
+    res.status(500).send("Server Error.");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
