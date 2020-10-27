4fbee62505eaae4356e4b8320ff1f7c9107f9ee8
Shuyang He <maxhe2019@gmail.com>
2020-10-27 01:16:34 -0700

    about content change

--- src/components/About/AboutData.js
+++ src/components/About/AboutData.js
@@ -1,7 +1,7 @@
 const data = {
   name: "Shuyang He",
   contact: "maxhe2019@gmail.com     206-849-9105",
-  content: "A front end developer experienced in MVVM frameworks, responsive web design, RESTful API and Microservices",
+  content: "A front end developer experienced in MVVM frameworks, responsive web design, RESTful API and Microservices.",
 };
 
 export default data;
