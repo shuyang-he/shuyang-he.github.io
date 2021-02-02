c05b37a24a97b3f08dbfa3b75ccb6681faf06b9c
Shuyang He <maxhe2019@gmail.com>
2021-02-01 18:25:23 -0800

    change about

--- src/components/About/AboutData.js
+++ src/components/About/AboutData.js
@@ -1,7 +1,7 @@
 const data = {
   name: "Shuyang He",
   contact:
-    "(206) 849-9105     maxhe2019@gmail.com     San Jose, California 95133",
+    "(206) 849-9105 ｜ maxhe2019@gmail.com ｜ San Jose, California 95133",
   content:
     "A front end developer experienced in MVVM frameworks, responsive web design, RESTful API design.",
 };
